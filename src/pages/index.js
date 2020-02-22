475372e8e6fc94c95e3be1cebe93b84349448606
justingarcianm <justingarcianm@gmail.com>
2020-02-21 22:16:21 -0700

    add /blog and Markdown support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+export default () => {
+    return (
+        <>
+        Hello World
+        <p><Link to="/blog">View Blog</Link></p>
+        </>
+    )
+}
